609c7198a2fce11239c645f95ae9c87bf9342593
Edward Burke <ed@burkessf.com>
2015-04-29 14:27:57 -0700

    Added posts.py

--- application/posts.py
+++ application/posts.py
@@ -3,5 +3,12 @@ from collections import User, Posts
 from flask import session, render_template, redirect, flash, url_for, request
 
 @app.route('/post')
-def Posts():
-    
+def posts():
+
+    if not session.get('logged_in'):
+        return redirect(url_for('login')) '''If the user tries to post and is not logged in, redirects them to login. '''
+
+    body = request.form["data"]
+    title = request.form["title"]
+    p = Posts(author = user, title = title, content = body)
+    p.save() 

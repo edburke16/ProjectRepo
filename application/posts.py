6958c909e53fab409a6bf5abb1645fdaee4619eb
Edward Burke <ed@burkessf.com>
2015-05-05 12:43:45 -0700

    More work done on posts, needs HTML for testing.

--- application/posts.py
+++ application/posts.py
@@ -1,16 +1,16 @@
-from applications import app
+from application import app
 from collections import User, Posts
 from flask import session, render_template, redirect, flash, url_for, request
 
-@app.route('/post')
-def posts():
-
+@app.route('/post', methods=["POST"])
+def make_posts():
     if not session.get('logged_in'):
-        return redirect(url_for('login')) '''If the user tries to post and is not logged in, redirects them to login. '''
+        return redirect(url_for('login'))
+#If the user tries to post and is not logged in, redirects them to login.
 
     body = request.form["data"]
     title = request.form["title"]
     p = Posts(author = user, title = title, content = body)
     p.save()
 
-    return render_template('post.html')
+    return redirect(url_for('post', uid=p.id))
